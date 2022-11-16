60da6f047fc90418ff4faf69f05a34a4f8e3614c
Your Name <you@example.com>
2022-11-16 08:16:33 +0530

    Added: Welcome to Address Book Program

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        /**
+         * create a main method ,all program execute in main method
+         * 1st print welcome msg.
+         */
+        System.out.println("Welcome to Address Book System\n");
+    }
+}
