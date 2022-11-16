48127ac7e78f58835939e8ffbbe55e955e493459
Your Name <you@example.com>
2022-11-16 08:29:25 +0530

    Added: Welcome to Address Book Program

--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -84,20 +84,3 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 }
-    /**
-     * We are just calling an object using class name.
-     * Simply passing the argument value according to the parameter defined in the constructor.
-     */
-    Contact details = new Contact("Poonam", "Desai", "Shirol", "Shirol",
-            "Maharashtra", "loharpoonam98@gmail.com", 416103, 7058835985l);
-            System.out.println("The following contact details is mentioned below : \n");
-                    System.out.println("First Name : " + details.getFirstName());
-                    System.out.println("Last Name  : " + details.getLastName());
-                    System.out.println("Address    : " + details.getAddress());
-                    System.out.println("City       : " + details.getCity());
-                    System.out.println("State      : " + details.getState());
-                    System.out.println("E-mail     : " + details.getEmail());
-                    System.out.println("Zip Code   : " + details.getZip());
-                    System.out.println("Phone No   : " + details.getPhoneNumber());
-                    }
-                    }
