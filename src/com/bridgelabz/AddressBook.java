dbee3b17bd8f23e3d3f0a7fe9cffbcd7415f3681
Your Name <you@example.com>
2022-11-17 12:24:33 +0530

    Added: logic for create contacts in address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -6,5 +6,19 @@ public class AddressBook {
 
         System.out.println("Welcome to Address Book System\n");
 
+        /**
+         * We are just calling an object using class name.
+         * Simply passing the argument value according to the parameter defined in the constructor.
+         */
+
+        Contacts info = new Contacts("Poonam", "Desai", "Shirol", "Shirol", "Maharashtra", "loharpoonam98@gmail.com", 416103, 7058835985l);
+        System.out.println("Firstname: " + info.getFirstName());
+        System.out.println("Lastname: " + info.getLastName());
+        System.out.println("Address: " + info.getAddress());
+        System.out.println("City: " + info.getCity());
+        System.out.println("State: " + info.getState());
+        System.out.println("Email: " + info.getEmail());
+        System.out.println("Zip: " + info.getZip());
+        System.out.println("PhoneNumber: " + info.getPhoneNumber());
     }
 }
